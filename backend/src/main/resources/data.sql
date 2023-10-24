bda921018de76012f3b4117c3b9725b5014aad29
julianp950 <julian.p950@gmail.com>
2023-10-24 16:19:49 -0300

    first version bugs expected

--- backend/src/main/resources/data.sql
+++ backend/src/main/resources/data.sql
@@ -3,3 +3,5 @@ INSERT INTO role (name, permission_create, permission_update, permission_delete)
 INSERT INTO role (name, permission_create, permission_update, permission_delete) VALUES ('user', false, true, false);
 
 INSERT INTO users (name, user_name, password, age, role_id) VALUES ('Administrator', 'admin', '$2a$10$NRQfwftsX2hHFGmvUeA26.YMGgvDR/kNk660Snwh8G93khvnTwUqG', 0, 1);
+INSERT INTO users (name, user_name, password, age, role_id) VALUES ('Viewer', 'viewer', '$2a$10$vEsHp/Z8/ooVaN0xOLb76eXRPIlbZrqg3xvUJR9FufnRTJvuSIqZe', 0, 2);
+INSERT INTO users (name, user_name, password, age, role_id) VALUES ('User', 'user', '$2a$10$pA..qv9IpqKIm0.kp0m2Me4YGYeOyjbXGXzyAjZJmBlAiSmi3zaqa', 0, 3);
